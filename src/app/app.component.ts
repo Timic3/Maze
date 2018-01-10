ae85cf7a37d120b0803c919c381650e6ec90e086
Timotej <timotej.manfreda@gmail.com>
2018-01-10 23:04:03 +0100

    - Added GlslCanvas script - Added awesome Galaxy shader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, AfterContentInit } from '@angular/core';
+
+// Find another way to link it with NPM
+declare var GlslCanvas: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements AfterContentInit {
+  private glslSandbox: any;
+
+  ngAfterContentInit() {
+    // Construct GLSL sandbox
+    const galaxy = document.getElementById('galaxyBackground');
+    this.glslSandbox = new GlslCanvas(galaxy);
+
+    // Update canvas based on window size
+    this.windowUpdate();
+    window.addEventListener('resize', this.windowUpdate, false);
+  }
+
+  // To retain scope (this) in event listener, I use Lambda expression
+  private windowUpdate = () => {
+    const galaxy = document.getElementById('galaxyBackground');
+    galaxy.setAttribute('width', window.innerWidth + '');
+    galaxy.setAttribute('height', window.innerHeight + '');
+    // Update GL viewport to fit the canvas
+    this.glslSandbox.gl.viewport(0, 0, this.glslSandbox.gl.canvas.width, this.glslSandbox.canvas.height);
+  }
 }
